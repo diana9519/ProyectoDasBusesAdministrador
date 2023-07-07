f8cfd63b1821067804586bc7f6be166ef99926b6
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-07-06 20:49:06 -0500

    PD-123-Integración Servicios Frecuencias App Web - ANT

---
+++ frontwebadmin/views/modules/frecuenciasadmin.php
@@ -0,0 +1,120 @@
+<head>
+  <script>
+    function editarFrecuencia(id_frecuencia, origen_frecuencia, destino_frecuencia, costo_frecuencia, duracion_frecuencia) {
+      var encodedId = encodeURIComponent(id_frecuencia);
+      var encodedOrigen = encodeURIComponent(origen_frecuencia);
+      var encodedDestino = encodeURIComponent(destino_frecuencia);
+      var encodedCosto = encodeURIComponent(costo_frecuencia);
+      var encodedDuracion = encodeURIComponent(duracion_frecuencia);
+      var redirectUrl = 'redireccionadmin.php?action=frecuenciasform&id_frecuencia=' + encodedId +
+        '&origen_frecuencia=' + encodedOrigen +
+        '&destino_frecuencia=' + encodedDestino +
+        '&costo_frecuencia=' + encodedCosto +
+        '&duracion_frecuencia=' + encodedDuracion;
+
+      window.location.href = redirectUrl;
+    }
+  </script>
+  
+</head>
+
+<body>
+  <div class="indexStyleTitulo">
+    <br><br><br>
+    <div style="padding-left: 30px; padding-right: 30px; padding-top: 15px;">
+      <h3 style="font-size: 20px;">
+        <span style="display: inline-block; vertical-align: middle;">Nueva Frecuencia</span>
+        <a href="redireccionadmin.php?action=frecuenciasform">
+          <img class="iconos" src="img/mas.png">
+        </a>
+      </h3>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Origen</th>
+            <th scope="col">Destino</th>
+            <th scope="col">Costo</th>
+            <th scope="col">Duración</th>
+            <th scope="col">Acciones </th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $url = 'https://nilotic-quart.000webhostapp.com/listarFrecuencias.php';
+
+
+          $ch = curl_init($url);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+          $response = curl_exec($ch);
+
+          if ($response !== false) {
+            $data = json_decode($response, true);
+
+            if (!empty($data)) {
+              echo '<script>
+                                    var cityMap = {
+                                      1: "Cuenca",
+                                      2: "Guaranda",
+                                      3: "Azogues",
+                                      4: "Tulcán",
+                                      5: "Riobamba",
+                                      6: "Latacunga",
+                                      7: "Machala",
+                                      8: "Esmeraldas",
+                                      9: "Guayaquil",
+                                      10: "Ibarra",
+                                      11: "Loja",
+                                      12: "Babahoyo",
+                                      13: "Portoviejo",
+                                      14: "Macas",
+                                      15: "Tena",
+                                      16: "Orellana",
+                                      17: "Puyo",
+                                      18: "Quito",
+                                      19: "Santa Elena",
+                                      20: "Santo Domingo",
+                                      21: "Nueva Loja",
+                                      22: "Ambato",
+                                      23: "Zamora",
+                                      24: "Salcedo",
+                                      25: "La Maná"
+                                    };
+
+                                    $(document).ready(function() {
+                                      var cities = $(".city");
+
+                                      cities.each(function() {
+                                        var cityId = $(this).data("id");
+                                        var cityName = cityMap[cityId];
+                                        $(this).text(cityName);
+                                      });
+
+                        
+                                    });
+                                </script>';
+
+              foreach ($data as $frecuencia) {
+                echo '<tr>';
+                echo '<td class="city" data-id="' . $frecuencia['origen_frecuencia'] . '"></td>';
+                echo '<td class="city" data-id="' . $frecuencia['destino_frecuencia'] . '"></td>';
+                echo '<td>' . $frecuencia['costo_frecuencia'] . '</td>';
+                echo '<td>' . $frecuencia['duracion_frecuencia'] . '</td>';
+                echo '<td>';
+                echo '<img class="iconos" src="img/editar.png" onclick="editarFrecuencia(\'' . $frecuencia['id_frecuencia'] . '\', \'' . $frecuencia['origen_frecuencia'] . '\', \'' . $frecuencia['destino_frecuencia'] . '\', \'' . $frecuencia['costo_frecuencia'] . '\',\'' . $frecuencia['duracion_frecuencia'] . '\')">';
+                echo '</td>';
+                echo '</tr>';
+              }
+            } else {
+              echo '<tr><td colspan="5">No se encontraron registros en la tabla</td></tr>';
+            }
+          } else {
+            echo '<tr><td colspan="5">Error al obtener los datos</td></tr>';
+          }
+          curl_close($ch);
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
---
+++ frontwebadmin/views/templateadmin.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAFEY! - Inicio</title>
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="easyui/themes/default/easyui.css">
+    <link rel="stylesheet" type="text/css" href="easyui/themes/icon.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="easyui/jquery.min.js"></script>
+    <script type="text/javascript" src="easyui/jquery.easyui.min.js"></script>
+    
+</head>
+
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark;">
+            <div class="container-fluid" style="display:flex;">
+            <a class="navbar-brand" href="redireccionadmin.php?action=home">
+                    <img src="img/safey.jpg" class="avatar"> </a>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    </ul>
+                    <a class="navbar-brand" href="redireccionadmin.php?action=perfilusuario" style="justify-content:right">
+                    <img src="img/avatar.png" class="avatar" onclick=animate> </a>
+                <button type="button" class="btn btn-danger; buttonStyle" id="btnLogout">Cerrar Sesión</button>
+            </div>
+        </nav>
+    </div>
+    <div>
+        <nav class="navbar-expand-lg navbar-dark navBg custom-nav">
+            <div class="container-fluid">                
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active; navTemplate" href="redireccionadmin.php?action=cooperativas">Cooperativas</a>
+                    </li>                
+                    <li class="nav-item">
+                        <a class="nav-link active; navTemplate"
+                            href="redireccionadmin.php?action=frecuencias">Frecuencias</a>
+                    </li>
+                </ul>
+                
+
+            </div>
+        </nav>
+    </div>
+    <section>
+        <?php
+        $mvc = new MvcController();
+        $mvc->enlacesPaginasControllerAdmin();
+        ?>
+    </section>
+
+    <footer class="footerStyle">
+        <p>
+            <br>
+            <b>______________________________________</b>
+            <a href="">
+                <img src="img/mail.png" class="socialnetwork">
+            </a>
+            <b>_</b>
+            <a href="">
+                <img src="img/instagram.png" class="socialnetwork">
+            </a>
+            <b>_</b>
+            <a href="">
+                <img src="img/twitter.png" class="socialnetwork">
+            </a>
+            <b>_</b>
+            <a href="">
+                <img src="img/facebook.png" class="socialnetwork">
+            </a>
+            <b>______________________________________</b>
+            <br>
+            <b>Copyright © 2023 Safey</b>
+            <br>
+            <b><a href="redireccionadmin.php?action=home" style="color:white">Inicio</a> | <a href=""
+                    style="color:white">Acerca de</a></b>
+        </p>
+    </footer>
+</body>
+
+
+</html>
+<script>
+    var boton = document.getElementById("btnLogout");
+    boton.onclick = function () {
+        window.location = "logout.php";
+    }
+</script>
